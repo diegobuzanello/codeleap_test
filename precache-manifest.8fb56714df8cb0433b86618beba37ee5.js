c79a2baf4cb1420e3776cf45677dc8f06f42cb97
DiegoBuzanello - Desktop <buzanello.diego@gmail.com>
2020-11-06 23:10:36 -0300

    Updates

---
+++ precache-manifest.8fb56714df8cb0433b86618beba37ee5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1994ee48329bf09cefee7fe8b076ba2e",
+    "url": "/codeleap_test/index.html"
+  },
+  {
+    "revision": "06b407e24064d8a17c98",
+    "url": "/codeleap_test/static/css/main.6f7601a8.chunk.css"
+  },
+  {
+    "revision": "9b8089ea978ca4382fe0",
+    "url": "/codeleap_test/static/js/2.08623810.chunk.js"
+  },
+  {
+    "revision": "7c514825c1a9c4a61543a783549fbf2b",
+    "url": "/codeleap_test/static/js/2.08623810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b407e24064d8a17c98",
+    "url": "/codeleap_test/static/js/main.b60ba81a.chunk.js"
+  },
+  {
+    "revision": "d2d34219774ac43892ad",
+    "url": "/codeleap_test/static/js/runtime-main.a5211df1.js"
+  },
+  {
+    "revision": "def4760f1a3ea523dae355761490517f",
+    "url": "/codeleap_test/static/media/bx_bx-edit.def4760f.svg"
+  },
+  {
+    "revision": "ac3755f2733ce3183153fd913b335af0",
+    "url": "/codeleap_test/static/media/codeleap_logo_black.ac3755f2.png"
+  },
+  {
+    "revision": "b3bd60bcc388d8e1796ff56bca4709ce",
+    "url": "/codeleap_test/static/media/ic_baseline-delete-forever.b3bd60bc.svg"
+  }
+]);
